cd427f91aed2377c6f1445316526b8a43d9ee845
Daniel G <yanimames>
2023-06-20 16:47:06 -0500

    updat so far

--- node-express-course-main/03-task-manager/starter/app.js
+++ node-express-course-main/03-task-manager/starter/app.js
@@ -1,1 +1,17 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express()
+const tasks = require ('./routes/task')
+
+//middleware
+app.use(express.json())
+
+//routes
+app.get('/hello', (req,res) => {
+    res.send('Task Manager App')
+})
+
+app.use('/api/v1/task',tasks)
+
+const port = 3000
+
+app.listen(port, console.log(`server is listening on port ${port}`))
